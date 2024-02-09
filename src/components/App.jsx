0a7510332255f7f6456b999996eba6f1378e4dd6
Tailon Dow <dow.tailon@gmail.com>
2024-02-08 21:59:51 -0600

    added a vercel.json file. Looking to get vercel to display other routes besides the home route.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { HashRouter as Router, Routes, Route } from "react-router-dom"; // Import HashRouter instead of BrowserRouter
 import "../css/App.css";
 import Home from "./Homepage/home.jsx";
 import VideoLib from "./Library_page/vid_lib.jsx";
@@ -7,19 +6,30 @@ import HoneeBio from "./HoneeBee_Bio/HoneeBio.jsx";
 import ButterflyBio from "./Butterfly_Bio/ButterflyBio.jsx";
 import Contact from "./Contact/contact_main";
 
+import {
+    createRoutesFromElements,
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
+
+const routes = createRoutesFromElements(
+    <Route>
+        <Route path="/" element={<Home />} />
+        <Route path="/library" element={<VideoLib />} />
+        <Route path="/honee" element={<HoneeBio />} />
+        <Route path="/butterfly" element={<ButterflyBio />} />
+        <Route path="/contact" element={<Contact />} />
+    </Route>
+);
+
+const router = createBrowserRouter(routes);
+
 function App() {
     return (
-        <Router>
-            {" "}
-            {/* Use HashRouter instead of BrowserRouter */}
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/library" element={<VideoLib />} />
-                <Route path="/honee" element={<HoneeBio />} />
-                <Route path="/butterfly" element={<ButterflyBio />} />
-                <Route path="/contact" element={<Contact />} />
-            </Routes>
-        </Router>
+        <>
+            <RouterProvider router={router} />
+        </>
     );
 }
 
